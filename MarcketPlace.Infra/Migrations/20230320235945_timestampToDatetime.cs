f21152d828aa7ca35a121d8966f17d77ebaf5631
felipebsoares <103378305+felipebsoares@users.noreply.github.com>
2023-03-20 21:11:49 -0300

    feat: pretend it works

---
+++ MarcketPlace.Infra/Migrations/20230320235945_timestampToDatetime.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MarcketPlace.Infra.Migrations
+{
+    public partial class timestampToDatetime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CriadoEm",
+                table: "Fornecedores",
+                type: "datetime",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(byte[]),
+                oldType: "timestamp",
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "AtualizadoEm",
+                table: "Fornecedores",
+                type: "datetime",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(byte[]),
+                oldType: "timestamp",
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CriadoEm",
+                table: "Clientes",
+                type: "datetime",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(byte[]),
+                oldType: "timestamp",
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "AtualizadoEm",
+                table: "Clientes",
+                type: "datetime",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(byte[]),
+                oldType: "timestamp",
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CriadoEm",
+                table: "Administradores",
+                type: "datetime",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(byte[]),
+                oldType: "timestamp",
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "AtualizadoEm",
+                table: "Administradores",
+                type: "datetime",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(byte[]),
+                oldType: "timestamp",
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "CriadoEm",
+                table: "Fornecedores",
+                type: "timestamp",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "AtualizadoEm",
+                table: "Fornecedores",
+                type: "timestamp",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "CriadoEm",
+                table: "Clientes",
+                type: "timestamp",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "AtualizadoEm",
+                table: "Clientes",
+                type: "timestamp",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "CriadoEm",
+                table: "Administradores",
+                type: "timestamp",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "AtualizadoEm",
+                table: "Administradores",
+                type: "timestamp",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+        }
+    }
+}
