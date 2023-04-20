6f0205ad2e2e2fcc296d6782208f4af0d810a924
NatanLima62 <natanlimaoliveira62@gmail.com>
2023-04-19 22:43:24 -0300

    migration

---
+++ MarcketPlace.Infra/Migrations/20230420013106_AddPrecoParaProdutoServico.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MarcketPlace.Infra.Migrations
+{
+    public partial class AddPrecoParaProdutoServico : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Preco",
+                table: "ProdutoServicos",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "PrecoDesconto",
+                table: "ProdutoServicos",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Preco",
+                table: "ProdutoServicos");
+
+            migrationBuilder.DropColumn(
+                name: "PrecoDesconto",
+                table: "ProdutoServicos");
+        }
+    }
+}
